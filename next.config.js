ae7aa8aef8a96d6020b60ac097f517ed75164ff1
FeroHriadel <ferdinand.hriadel@gmail.com>
2021-06-11 12:32:40 +0200

    modified next.config.js with target: serverless

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     env: {
       api: "https://murmuring-taiga-44093.herokuapp.com/api"
-    }
+    },
+    target: "serverless"
 };
 
 //development api: "http://localhost:5000/api"
